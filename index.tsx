00e60983d615f5a7a74f73d8715d2fa0d1d493ad
naineshsalvi <naineshsalvi@gmail.com>
2026-01-31 18:39:14 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,21 +1,4 @@
-<h1 style="color:white; text-align:center; margin-top:40px;">
-  WEBSITE IS DEPLOYED CORRECTLY
-</h1>
-<p style="color:white; text-align:center;">
-  Content is hidden by JavaScript
-</p>
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Static HTML Project - JavaScript Frameworks Removed
+// The application now runs directly from index.html
+console.log("Sawant's Kitchen: Static HTML Version Loaded Successfully.");
